080f765debee80127bce869661b1f41b462784d6
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-10 15:50:59 +0100

    Added missing param doc

--- src/TypeInterface.php
+++ src/TypeInterface.php
@@ -16,9 +16,10 @@ interface TypeInterface extends ValidatorInterface
      *
      * @since [*next-version*]
      *
-     * @param bool $strict Whether or not comparison will be done in strict mode.
-     *                     In this mode, the value type must match exactly. Otherwise, validation
-     *                     will determine if the variable can be cast to this type.
+     * @param mixed $value  The value to validate against the type.
+     * @param bool  $strict Whether or not comparison will be done in strict mode.
+     *                      In this mode, the value type must match exactly. Otherwise, validation
+     *                      will determine if the variable can be cast to this type.
      */
     public function validate($value, $strict = true);
 }
